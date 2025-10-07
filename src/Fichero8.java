0fb9503c1d5d30542ac7d3adb4b89dcde4df3aab
sergiogitdam <sergiodamsegundo@gmail.com>
2025-10-07 16:42:21 +0200

    07/10/2025 - Fichero 8 terminado, ayer no hice ninguno porque estuve ayudando a mi compañero a que entendiese el 7 :)

--- src/Fichero8.java
+++ src/Fichero8.java
@@ -1,6 +1,7 @@
-import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.Scanner;
 import java.io.File;
+import java.net.URI;
 
 /*
 Función explorarCarpeta(String ruta): lista contenido.
@@ -11,9 +12,44 @@ Practica: list(), isFile(), isDirectory(), toURI().
 public class Fichero8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("***EXPLORADOR DE CARPETAS***");
+        explorarCarpeta(sc);
+        sc.close();
     }
 
-    public static void explorarCarpeta(String ruta) {
+    public static void explorarCarpeta(Scanner sc)  {
+        System.out.print("Introduce la ruta del directorio que quieras explorar: ");
+        String rutaCarpeta = sc.nextLine();
+        File ruta = new File(rutaCarpeta);
+        if (ruta.exists()) {
+            for (String nombreElemento  : ruta.list()) {
+                File elementoEnArray = new File(ruta, nombreElemento);
+                analizarElemento(ruta, elementoEnArray, nombreElemento);
+            }
+            convertirAURI(ruta);
+        } else {
+            System.out.println("La ruta introducida es errónea o no existe en el equipo");
+        }
+    }
 
+    public static void analizarElemento(File ruta, File elementoEnArray, String nombreElemento) {
+        if (elementoEnArray.isFile()) {
+            long pesoArchivoEnBytes = elementoEnArray.length();
+            System.out.println("Archivo: " + nombreElemento + " [Peso del archivo: " + pesoArchivoEnBytes + " bytes]");
+        } else if (elementoEnArray.isDirectory()) {
+            String[] elementos = elementoEnArray.list();
+            System.out.println("Directorio: " + nombreElemento + " [Número de elementos del directorio: " + elementos.length + "]");
+        }
+    }
+
+    public static void convertirAURI(File ruta) {
+        System.out.println("Intentanto convertir la ruta '" + ruta + "' a URI");
+        try {
+            URI uri = ruta.toURI();
+            System.out.println("Ruta convertida a URI correctamente: " + uri.toString());
+        } catch (Exception e) {
+            // Mensaje de error en caso de fallo de conversión
+            System.err.println("Error al intentar convertir la ruta a URI: " + e.getMessage());
+        }
     }
 }
